55a8bffaa376941350f57f832b2d46ef55d3bac7
kevinalonso <k.alonso@iia-laval.fr>
2022-05-01 22:03:57 +0200

    cookies mis en place

--- src/js/site.js
+++ src/js/site.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	if (document.cookie_consent != undefined) {
 		$('html').css({overflow: 'hidden'});
+		h = screen.height + 100;
+		document.getElementsByClassName("ch-cookie-consent")[0].style.height = h+'px'
 	}
 });
